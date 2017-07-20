441b1704d5f8d265b91f34092f15ab67442862d4
Natiq Vahabov <natig@MBP-Natig.mshome.net>
2017-07-20 11:46:06 +0400

    WElcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laraveladsdsaadssadads sassaadssad
+                    Laravel
                 </div>
 
                 <div class="links">
